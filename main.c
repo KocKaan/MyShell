64570bad0b1dd67807532893b24a0f76e94a20aa
KocKaan <kk4291@nyu.edu>
2022-09-28 03:19:46 -0400

    Input prompt

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
-int main(int argc, const char *const *argv)
+// declare variable
+static char *args[512];
+static char prompt[512];
+char *inputBuff;
+char cwd[1024];
+
+// declare functions
+void createPrompt();
+
+void reset()
 {
+    cwd[0] = '\0';
+    prompt[0] = '0';
+}
+
+void createPrompt()
+{
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        strcpy(prompt, "Project shell> ");
+        strcat(prompt, cwd);
+        strcat(prompt, ":$ ");
+    }
+    else
+    {
+
+        perror("Error in getting curent working directory: ");
+    }
+    return;
+}
+
+int main()
+{
+    int counter = 1;
+
+    do
+    {
+        reset();
+        createPrompt();
+        inputBuff = readLine(prompt);
+
+        if (!strncmp(inputBuff, "exit"))
+
+    } while (counter);
+
     printf("kaan\n");
 }
